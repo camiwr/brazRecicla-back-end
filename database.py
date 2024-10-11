ef33793ff7a86ab7faab10fae9928e26511272a1
Camilla Soares Sousa <camillasoares818@gmail.com>
2024-10-11 01:25:18 -0300

    Configurando as Variaveis de ambiente

--- database.py
+++ database.py
@@ -1,14 +1,17 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host="aws-0-sa-east-1.pooler.supabase.com",
-        database="postgres",  
-        user="postgres.zcbswpxewpxprtffxhza", 
-        port="6543",    
-        password="brazrecicla022", 
+        host=os.getenv("SUPABASE_HOST"),
+        database=os.getenv("SUPABASE_DB"),
+        user=os.getenv("SUPABASE_USER"),
+        password=os.getenv("SUPABASE_PASSWORD"),
+        port=os.getenv("SUPABASE_PORT"),
         cursor_factory=RealDictCursor
     )
     return conn
